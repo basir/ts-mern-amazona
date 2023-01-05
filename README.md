7919c1ab0ad7967f7400221d6326105b00f136ee
Bassir Jafarzadeh (Coding Instructor) <basir.jafarzadeh@gmail.com>
2023-01-05 17:03:37 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# TypeScript MERN AMAZONA
+
+## Lessons
+
+1. Introduction
+2. Install Tools
+3. Create TypeScript React App By Vite
+4. Create Git Repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <header>TS Amazona</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  )
+}
+
+export default App
